26902d76f32ec6662840620f19f81e5121e82438
DogeisCut <rickisparks06@gmail.com>
2024-02-16 14:59:36 -0700

    :P

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
+    //HELLO I AM A COMMENT
+    /*
+        I AM
+        A
+        MUlTILINE
+        COMMENT
+    */
     println!("Hello, world!");
+
+    let mut x = 4;
+    println!("x is: {}", x);
+    x = 5;
+    println!("x is: {}", x);
+
+    let y = 4;
+    println!("y is: {}", y);
+    let y = y + 1;
+    println!("y is: {}", y);
 }
